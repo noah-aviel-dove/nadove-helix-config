715823686ef7934bae33baed13aa8d6240f4ee0c
Noa Aviel Dove <noah.aviel.dove@gmail.com>
2026-04-12 17:03:45 -0700

    Source bashrc during install

--- install.bash
+++ install.bash
@@ -7,4 +7,5 @@ ln -sT $(pwd)/config/helix ~/.config/helix
 if ! grep config-helix-theme "$bashrc"; then
 	echo "export PATH=\$PATH:$(pwd)/src/" >> "$bashrc";
 	echo "alias hx='config-helix-theme && /usr/bin/hx'" >> "$bashrc";
+	. "$bashrc";
 fi
