9b92f85bce016cb9e5ff84e952784e92464f100f
Noa Aviel Dove <noah.aviel.dove@gmail.com>
2026-04-19 13:37:04 -0700

    Set RUST_BACKTRACE=1

--- exec/install.bash
+++ exec/install.bash
@@ -6,6 +6,6 @@ ln -sT $(pwd)/config/helix ~/.config/helix
 
 if ! grep config-helix-theme "$bashrc"; then
 	echo "export PATH=\$PATH:$(pwd)/exec/" >> "$bashrc";
-	echo "alias hx='config-helix-theme && /usr/bin/hx'" >> "$bashrc";
+	echo "alias hx='config-helix-theme && RUST_BACKTRACE=1 /usr/bin/hx'" >> "$bashrc";
 	. "$bashrc";
 fi
